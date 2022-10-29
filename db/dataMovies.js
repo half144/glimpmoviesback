b607e1977e8587ef79e384ce66046eb3e824460b
half144 <guilhermefeller2002@gmail.com>
2022-10-29 18:18:37 -0300

    make it better

---
+++ db/dataMovies.js
@@ -0,0 +1,25 @@
+const action = require("./action.json");
+const adventure = require("./adventure.json");
+const comedy = require("./comedy.json");
+const crime = require("./crime.json");
+const drama = require("./drama.json");
+const fantasy = require("./fantasy.json");
+const romance = require("./romance.json");
+const mistery = require("./mistery.json");
+const terror = require("./terror.json");
+const allItems = require("./allItems.json");
+const series = require("./series.json");
+
+module.exports = {
+  action,
+  adventure,
+  comedy,
+  crime,
+  drama,
+  fantasy,
+  romance,
+  mistery,
+  terror,
+  allItems,
+  series,
+};
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const app = express();
+const db = require("./db/dataMovies");
+port = process.env.PORT || 80;
+
+const allMoviesLowerCase = db.allItems.map((movie) => ({
+  name: String(movie.name).toLowerCase(),
+  id: movie.id,
+  type: movie.type,
+  poster: movie.poster,
+}));
+
+app.listen(port, () => {
+  console.log("API ONLINE");
+});
+
+app.get("/action", (req, res) => {
+  res.json(db.action.slice(0, 80));
+});
+
+app.get("/romance", (req, res) => {
+  res.json(db.romance.slice(0, 80));
+});
+
+app.get("/adventure", (req, res) => {
+  res.json(db.adventure.slice(0, 80));
+});
+
+app.get("/crime", (req, res) => {
+  res.json(db.crime.slice(0, 80));
+});
+
+app.get("/comedy", (req, res) => {
+  res.json(db.comedy.slice(0, 80));
+});
+
+app.get("/drama", (req, res) => {
+  res.json(db.drama.slice(0, 80));
+});
+
+app.get("/mistery", (req, res) => {
+  res.json(db.mistery.slice(0, 80));
+});
+
+app.get("/terror", (req, res) => {
+  res.json(db.terror.slice(0, 80));
+});
+
+app.get("/fantasy", (req, res) => {
+  res.json(db.fantasy.slice(0, 80));
+});
+
+app.get("/getall", (req, res) => {
+  res.json(allMoviesLowerCase);
+});
+
+app.get("/series", (req, res) => {
+  res.json(db.series);
+});
